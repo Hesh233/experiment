b0970399f92dfa84c66c29d38e9b80805bb8a575
Hesh233 <916933927@qq.com>
2018-12-02 22:41:43 +0800

    Update bilibiliSpider.py

--- spider/bilibiliSpider.py
+++ spider/bilibiliSpider.py
@@ -10,10 +10,12 @@ from xlutils.copy import copy
 from datetime import datetime
 #爬取最大的页数，0就是全爬完
 MAXPAGE_SIZE = 0
+#用于开始加载页数 0为不用
+PAGE_START = 2995
 #每次请求间隔时间,越小越快，但不稳定  0.3在700页左右断过一次 0.4在577页停过一次  0.2在157 最好0.5
 REFRESH_WAIT_TIME = 0.5
 #B站有评论的页面
-SPIDER_URL = 'https://www.bilibili.com/bangumi/play/ss25681'                      #https://www.bilibili.com/bangumi/play/ss25681  25510
+SPIDER_URL = 'https://www.bilibili.com/bangumi/play/ss21542'                      #https://www.bilibili.com/bangumi/play/ss25681  25510  GG 3450停掉了超出xls大小了
 class Bilibili(object):
     def __init__(self):
         #创建工作簿
@@ -32,11 +34,12 @@ class Bilibili(object):
         self.sheet.write(0,5,"客户端")
         self.countSum = 0        #总条数计数器
         self.forCount = 0       #for循环条数计数器
-    def run(self):
         #读取加载url
         self.driver.get(SPIDER_URL)   
+    def run(self):
+              
         #等待浏览器加载完成         
-        self.driver.implicitly_wait(3)
+        self.driver.implicitly_wait(10)
         #拉到底下获取评论
         js = "var q = document.documentElement.scrollTop=1000000"  
         self.driver.execute_script(js)  
@@ -78,15 +81,25 @@ class Bilibili(object):
             #print(name.__len__() ,text.__len__() ,textCount.__len__() ,commonTime.__len__(),showGood.__len__())
             self.countSum += self.forCount
             try:
-                self.driver.find_element_by_xpath('//div[@class="header-page paging-box"]/a[@class="next"]').click()
+                self.driver.find_element_by_xpath('//div[@class="bottom-page paging-box-big"]/a[@class="next"]').click()
             except:
                 break
             self.count += 1
             if self.count == MAXPAGE_SIZE:
                 print(self.countSum)
                 break
-        self.book.save("bili-test.xls")
+        self.book.save("bili-紫罗兰永恒花园2.xls")
         print("end")
+    def reload(self):
+        self.driver.implicitly_wait(10)
+        js = "var q = document.documentElement.scrollTop=1000000"  
+        self.driver.execute_script(js)  
+#         self.driver.find_element_by_xpath('//div[@class="page-jump"]/input').click()
+        self.driver.find_element_by_xpath('//div[@class="page-jump"]/input').send_keys(PAGE_START);
+        self.driver.find_element_by_xpath('//div[@class="page-jump"]/input').send_keys(Keys.ENTER)
+        time.sleep(3)
 if __name__=='__main__':
     bili = Bilibili()
+    if PAGE_START != 0:
+        bili.reload()
     bili.run()
